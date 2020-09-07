1c84f095d72f45ffeb6a0427f04e1774652984d9
yossy0201 <yossy0201msk@gmail.com>
2020-09-08 04:43:00 +0900

    guidance-page

---
+++ guidance.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@300&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <title>Guidance of lost child</title>
+</head>
+<body>
+  <header>
+    <h1 class="Guidance">迷子のご案内</h1>
+  </header>
+
+  
+  <div class="container">
+    <div id="img_unit">
+    </div>
+  </div>
+
+
+  <p class="names">お名前を入力してください</p>
+
+  <form>
+  <input type="text" name="name" class="text-area">
+  <input type="submit" value="検索する" class="serch-btn">
+  </form>
+
+  <p>名前：ピヨ子</p>
+  <p>場所：愛知：名古屋市中村区</p>
+  <p>特徴：人懐っこい、人が好き</p>
+  <img src="img/img1.jpg" alt="ピヨ子" title="ピヨ子" class="pic">
+
+  <p>リン</p>
+  <p>場所：岐阜：岐阜市</p>
+  <p>特徴：警戒心が強い、よく鳴く</p>
+  <img src="img/img2.jpg" alt="リン" title="リン" class="pic">
+
+  <p>ミー</p>
+  <p>場所：愛知：春日井市</p>
+  <p>特徴：木登りが好き</p>
+  <img src="img/img3.jpg" alt="ミー" title="ミー" class="pic">
+
+
+
+</body>
+</html>
