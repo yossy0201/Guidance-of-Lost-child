ab840bef836e6100c996a1fcd1d99cfde27db9bd
yossy0201 <yossy0201msk@gmail.com>
2020-09-08 05:11:26 +0900

    php-write

--- guidance.php
+++ guidance.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="css/style.css">
   <title>Guidance of lost child</title>
 </head>
+
 <body>
   <header>
     <h1 class="Guidance">迷子のご案内</h1>
@@ -18,30 +19,56 @@
     </div>
   </div>
 
-
-  <p class="names">お名前を入力してください</p>
+  <p class="names">
+  <?php
+  echo "お名前を入力してください"
+  ?>
+  </P>
+  <!-- <p class="names">お名前を入力してください</p> -->
 
   <form>
   <input type="text" name="name" class="text-area">
   <input type="submit" value="検索する" class="serch-btn">
   </form>
 
-  <p>名前：ピヨ子</p>
-  <p>場所：愛知：名古屋市中村区</p>
-  <p>特徴：人懐っこい、人が好き</p>
+  <div class="piyoko">
+  <?php
+  echo "名前：ピヨ子"
+  ?><br>
+  <?php
+  echo "場所：愛知：名古屋市中村区"
+  ?><br>
+  <?php
+  echo "特徴：人懐っこい、人が好き"
+  ?>
   <img src="img/img1.jpg" alt="ピヨ子" title="ピヨ子" class="pic">
+  </div>
 
-  <p>リン</p>
-  <p>場所：岐阜：岐阜市</p>
-  <p>特徴：警戒心が強い、よく鳴く</p>
+  <div class="rin">
+  <?php
+  echo "名前：リン"
+  ?><br>
+  <?php
+  echo "場所：岐阜：岐阜市"
+  ?><br>
+  <?php
+  echo "特徴：警戒心が強い、よく鳴く"
+  ?>
   <img src="img/img2.jpg" alt="リン" title="リン" class="pic">
+  </div>
 
-  <p>ミー</p>
-  <p>場所：愛知：春日井市</p>
-  <p>特徴：木登りが好き</p>
+  <div class="mi">
+  <?php
+  echo "名前：ミー"
+  ?><br>
+  <?php
+  echo "場所：愛知：春日井市"
+  ?><br>
+  <?php
+  echo "特徴：木登りが好き、冷静"
+  ?>
   <img src="img/img3.jpg" alt="ミー" title="ミー" class="pic">
-
-
+  </div>
 
 </body>
 </html>
